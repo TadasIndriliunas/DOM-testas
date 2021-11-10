118218c5c696690ebd777643eb19f8a0f8b27d3f
TadasIndriliunas <tadas.indriliunas@gmail.com>
2021-11-10 09:53:43 +0200

    6: fetch data

--- js/components/ApartmentGridComponent.js
+++ js/components/ApartmentGridComponent.js
@@ -6,9 +6,33 @@ class ApartmentGridComponent {
     };
     this.init();
   }
-  
+
+  saveApartment = Apartment => {
+    this.state = { Apartment, loading: false };
+
+    this.render();
+  }
+
+  showError = msg => alert(msg);
+
+  fetchApartment = () => API.fetchApartment(this.saveApartment, this.showError);
+
   init = () => {
+    this.state.loading = true;
+    this.fetchApartment();
     this.htmlElement = document.createElement('div');
-    this.htmlElement.innerHTML = "test";
+
+    this.render();
+  }
+
+  render = () => {
+    const { loading, Apartment } = this.state;
+    if (loading) {
+      this.htmlElement.innerHTML = 'Siunčiama...';
+    } else if (Apartment.length > 0) {
+      this.htmlElement.innerHTML = 'Parsiųsta!';
+    } else {
+      this.htmlElement.innerHTML = 'Atsiprašome, nėra elementų.';
+    }
   }
 }
